6bdf14d5c46c403307a0a98b40ce87cc84048b8f
kenny-nelson <taro.yamada.was.born.in.63.bce@gmail.com>
2015-07-08 22:27:13 +0900

    ObjectiveC 内で、C++を使えるようにするサンプル追加。

---
+++ Practices/SimpleObjectiveCpp/SimpleObjectiveCpp/main.mm
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World." << std::endl;
+
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
